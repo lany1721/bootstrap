68fe261cd467f3481a2556639d7e08118ba81b4d
Skiya <skiya.wu@cloudwise.com>
2021-12-24 15:05:00 +0800

    修改ToTimestampSerializer注释

--- src/main/java/cn/zpeace/bootstrap/config/jackson/ToTimestampSerializer.java
+++ src/main/java/cn/zpeace/bootstrap/config/jackson/ToTimestampSerializer.java
@@ -11,10 +11,13 @@ import java.time.ZoneId;
 import java.util.Date;
 
 /**
- * 把时间类型转换为时间戳
- * 用法，在时间类型成员属性标注注解 @JsonSerialize(using = ToTimestampSerializer.class)
- * 只支持：LocalDate、LocalDateTime、Date
- * 其他类型会抛出IllegalArgumentException
+ * 时间类型序列化时,序列化为时间戳
+ * <p>用法: </p>
+ * <li>单个字段:在时间类型成员属性标注注解 @JsonSerialize(using = ToTimestampSerializer.class)</li>
+ * <li>全局配置:使用 jackson 注册 module,如:module.addSerializer(LocalDateTime.class, new ToTimestampSerializer());</li>
+ *
+ *
+ * <p>只支持：LocalDate,LocalDateTime,Date. 其他类型会抛出IllegalArgumentException</p>
  *
  * @author skiya
  * @date Created on 2021-11-24.
@@ -24,7 +27,7 @@ public class ToTimestampSerializer extends StdSerializer<Object> {
     public ToTimestampSerializer() {
         super(Object.class);
     }
-    
+
     @Override
     public void serialize(Object value, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
         if (value == null) {
